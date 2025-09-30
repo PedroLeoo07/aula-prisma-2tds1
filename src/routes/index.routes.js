c15c01d9c46f7c82f893fce69dd27b593d9f3e02
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-09-30 09:59:39 -0300

    Feat: organiza rotas no servidor e integra o movieRouter

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-
-// Importar todas as rotas
+import movieRouter from "./movies.routes.js";
 
 const router = express.Router();
 
 // Rotas públicas
+router.use("/movies", movieRouter);
 
 export default router;
