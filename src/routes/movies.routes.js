8d4aeb539e38bc6d896c046bb65572d37a17cc16
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-09-30 09:55:27 -0300

    Feat: adiciona rotas para manipulação de filmes no MovieController

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import MovieController from "../controllers/movies.controller.js";
 
 const movieRouter = express.Router();
+
+movieRouter.get("/", MovieController.findAll);
+
+movieRouter.post("/", MovieController.create);
 
 export default movieRouter;
